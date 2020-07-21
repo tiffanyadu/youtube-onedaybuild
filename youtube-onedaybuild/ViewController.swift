0242819e6786cfb100128ae95c57c930e29ddb52
tiffanyadu <tadofdesign@gmail.com>
2020-07-21 09:03:41 -0500

    Initial Xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Tiffany Du on 7/21/20.
+//  Copyright © 2020 Tiffany Du. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
